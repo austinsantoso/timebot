184b19d47a0742eee8e16e4f4e55a688ea94a36b
Austin Santoso <48288091+austinsantoso@users.noreply.github.com>
2022-09-10 14:50:51 +0800

    Austin/init netlify deploy (#3)
    * init netlify logger
    
    * init netlify function

---
+++ netlify/functions/bot/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	log.Printf("Received value from telegram: %+v\n", request)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
